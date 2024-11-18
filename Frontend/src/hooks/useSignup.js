7566a4585e3090c665558b0716d00f1920b6150c
RahulVerma799 <160318545+RahulVerma799@users.noreply.github.com>
2024-11-18 16:22:16 +0530

    singup

--- Frontend/src/hooks/useSignup.js
+++ Frontend/src/hooks/useSignup.js
@@ -37,7 +37,7 @@ const useSignup = () => {
         try{
                 setLoading(true)
 
-                const response= await fetch("${BASE_URL}/api/signup",{
+                const response= await fetch(`${BASE_URL}/api/signup`,{
                     method:"POST",
                     headers:{"Content-Type":"application/json"},
                     body:JSON.stringify({
